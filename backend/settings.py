d71761607a0648d079b4a18b0e860e8f631cd96a
Raphael <chosenraphael1@gmail.com>
2025-05-21 15:18:30 +0100

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'backend.onrender.com',
+    'django-student-tp20.onrender.com',
 ]
 
 
@@ -161,7 +161,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
-MEDIA_URL='/media.'
+MEDIA_URL='/media./'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
